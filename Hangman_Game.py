9cbcdb97e7881e2a130ac9cfeac6165bcbe25a0a
anayatc@gmail.com <anayatc@sky.com>
2017-05-19 00:07:07 +0100

    fixing correct guesses

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -11,17 +11,22 @@ print(word)
 def hangman():
     guesses = len(word)
     word_list = [i for i in word]
+    dashes = '-' * len(word)
+    guessed = []
     while guesses > 0:
-        dashes = '-' * len(word)
         guess = input('Enter a letter: ').lower()
-        guessed = []
-        if guess not in word:
+        if guess in guessed:
+            print("You've already guessed that letter")
+        elif guess not in word:
             print(dashes)
+            print('Nope, that letter is not in the word')
             guessed.append(guess)
+            print(guessed)
             guesses -= 1
         elif guess in word:
             guessed.append(guess)
-            dashes = ''.join(['-' if guess != i and guess not in guessed else i for i in word_list])
+            print(guessed)
+            dashes = ''.join(['-' if guess != i and guess in guessed else i for i in word_list])
             print(dashes)
     return 'Game Over'
 
