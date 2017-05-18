ca51e54bea89e18ea64000a1adb5a0496939d109
anayatc@gmail.com <anayatc@sky.com>
2017-05-18 23:03:41 +0100

    fixing correct guesses

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -11,10 +11,13 @@ print(word)
 def hangman():
     guesses = len(word)
     while guesses > 0:
-        print('-' * len(word))
+        dashes = '-' * len(word)
+        print(dashes)
         guess = input('Enter a letter: ').lower()
         if guess not in word:
             guesses -= 1
+        elif guess in word:
+            return [i for i in word]
     return 'Game Over'
 
 
