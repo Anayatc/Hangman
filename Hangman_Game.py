80d029747e45245ed2d9c27e403cd2cf1f8fea56
anayatc@gmail.com <anayatc@sky.com>
2017-05-18 22:42:35 +0100

    added comments and function

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -1,9 +1,12 @@
 from random_words import RandomWords
+
 rw = RandomWords()
 
+# using the RandomWords Module selecting a random word and setting guesses to length of the random word
 word = ''.join(rw.random_words())
 guesses = len(word)
 print(word, guesses)
+
 
 def hangman():
     pass
