06e96c2dd1d3985160a038fdf9bf875fd99800ca
anayatc@gmail.com <anayatc@sky.com>
2017-05-18 22:49:20 +0100

    building function

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -4,9 +4,19 @@ rw = RandomWords()
 
 # using the RandomWords Module selecting a random word and setting guesses to length of the random word
 word = ''.join(rw.random_words())
-guesses = len(word)
-print(word, guesses)
+# guesses = len(word)
+print(word)
 
 
 def hangman():
-    pass
+    guesses = len(word)
+    while guesses > 0:
+        print('-' * guesses)
+        guess = input('Enter a letter: ').lower()
+        if guess not in word:
+            guesses -= 1
+
+
+
+
+print(hangman())
