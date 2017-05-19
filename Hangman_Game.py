e7a78b39f8ab4181c9c3d87115c91aaeff8913a4
anayatc@gmail.com <anayatc@sky.com>
2017-05-19 23:40:20 +0100

    added statement to let user know what they have already guessed

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -11,26 +11,27 @@ print(word)
 def hangman():
     guesses = len(word)
     word_list = [i for i in word]
-    dashes = '-' * len(word)
+    dashes = ['-' for i in word]
     guessed = []
     correct_guess = [i for i in guessed if i in word]
     while guesses > 0:
+        print(''.join(dashes))
         guess = input('Enter a letter: ').lower()
 
         if guess in guessed:
             print("You've already guessed that letter")
 
         elif guess not in word:
-            print(dashes)
+            print(''.join(dashes))
             print('Nope, that letter is not in the word')
             guessed.append(guess)
-            print(guessed)
+            print('letters you have guessed: ' + str(guessed))
             guesses -= 1
 
         elif guess in word:
             guessed.append(guess)
-            print(guessed)
-            dashes = ''.join(['-' for i in word_list if i not in guessed])
+            print('letters you have guessed: ' + guessed)
+            dashes = ''.join(dashes.replace(dashes[word_list.index(guess)], guess))
             print(dashes)
             print('Yes, you have guessed a correct letter')
 
